ab513c779e3d6603d52144067d4395a1a03ea6aa
Felipe Sere <felipesere@gmail.com>
2018-06-21 23:28:53 +0100

    Make filter menu collapsable

--- src/available-lessons.js
+++ src/available-lessons.js
@@ -1,11 +1,41 @@
 import React from 'react'
 import { Lesson } from './lesson/lesson'
 import { FilterMenu } from './filter-menu/filter-menu'
+import { Accordion, Icon } from 'semantic-ui-react'
+
+class CollapsableFilter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { open: false }
+  }
+
+  handleClick() {
+    this.setState({ open: !this.state.open })
+  }
+
+  render() {
+    return (
+      <Accordion fluid styled>
+        <Accordion.Title
+          active={this.state.open}
+          index={0}
+          onClick={this.handleClick.bind(this)}
+        >
+          <Icon name="dropdown" />
+          Filter Options
+        </Accordion.Title>
+        <Accordion.Content active={this.state.open}>
+          <FilterMenu />
+        </Accordion.Content>
+      </Accordion>
+    )
+  }
+}
 
 export function AvailableLessons({ lessons }) {
   return (
     <React.Fragment>
-      <FilterMenu />
+      <CollapsableFilter />
       <div className="available-lessons">
         {lessons.map(lesson => (
           <ModalOrLesson lesson={lesson} key={lesson.id} />
